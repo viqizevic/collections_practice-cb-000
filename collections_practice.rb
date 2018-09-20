61be4e044c0cc007f1592e254965376e82426b04
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-20 23:14:03 +0200

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,5 +4,36 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort.reverse
+  array.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a,b| a.size <=> b.size }
+end
+
+def swap_elements(array)
+  temp = array[2]
+  array[2] = array[1]
+  array[1] = temp
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect { |e| e = "#{e.slice(0..1)}$#{e.slice(3..(e.size-1))}"}
+end
+
+def find_a(array)
+  array.select { |e| e.start_with?('a') }
+end
+
+def sum_array(array)
+  array.inject(0) { |result, element| result + element }
+end
+
+def add_s(array)
+  array.each_with_index.collect{ |e, i| i != 1 ? "#{e}s" : e }
 end
